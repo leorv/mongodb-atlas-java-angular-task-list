21da91c0d33e1fe5eafd628163e52632977d494e
Leonardo Ruoso Vendramini <leonardo_vendramini@hotmail.com>
2022-12-01 14:02:14 -0300

    added angular components and routes.

--- task-app/src/app/app-routing.module.ts
+++ task-app/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { 
+        path: 'about', component: AboutComponent
+    },
+    {
+        path: 'tasks',
+        loadChildren: () => import('./tasks/tasks.module').then(m => m.TasksModule)
+    },
+    {
+        path: '',
+        component: HomeComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
